002d5629b08c4892f67cf001e7c4041e207ec5cf
Juan Facundo Suárez <facundo@svarez.com.ar>
2013-10-11 13:21:27 -0300

    improving code

--- index.js
+++ index.js
@@ -9,6 +9,14 @@
     var Apellido = require('./lastnametemplate');
     faco = document.getElementById('placeholder');
     faco.innerHTML = Apellido;
+    document.getElementById('apellido').onclick = nombre;
+  }
+
+  function nombre() {
+    var Nombre = require('./nametemplate');
+    faco = document.getElementById('placeholder');
+    faco.innerHTML = Nombre;
+    document.getElementById('name').onclick = apellido;
   }
 
   document.getElementById('name').onclick = apellido;
