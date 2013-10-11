6dae4c5c5cd7dd97fd50fae5b2b30975690c7b86
Juan Facundo Suárez <facundo@svarez.com.ar>
2013-10-11 11:40:10 -0300

    think worked

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
   console.log('JQuery loaded');
 
 
+  var Parrafo = require('./template');
+  console.log('-> Parrafo -> ', Parrafo);
+
+  faco = document.getElementById('placeholder');
+  faco.innerHTML = Parrafo;
